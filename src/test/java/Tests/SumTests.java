111c76671081275c812f9e068fd62bd085b8f52d
Андрей <vsscomp@mail.ru>
2026-02-08 15:48:35 +0300

    добавлены тесты и README.md

--- src/test/java/Tests/SumTests.java
+++ src/test/java/Tests/SumTests.java
@@ -2,11 +2,17 @@ package Tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static interfaces.INameTags.*;
+
+@Tag(REGRESS)
+@Tag(SUM)
 public class SumTests {
 
     @Test
+    @Tag(SMOKE)
     @DisplayName("Результат суммы 1")
     public void sum1() {
         int a = 1;
